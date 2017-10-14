4245fcd67a35ad869706f74f5a73779714071420
wang_szhan <wangshizhan1993@163.com>
2017-10-14 17:36:29 +0800

    1

--- www/templates/home/componentList/details/js/footer.js
+++ www/templates/home/componentList/details/js/footer.js
@@ -15,14 +15,18 @@
 			$scope.dataList = data;
 		}
 		publicService.receiveJson(getData, $scope.componentData);
+		
+		function getData1(data) {
+			$scope.buttonDataAll = data;
+		}
+		publicService.receiveJson(getData1, "buttonData");
 
-		$scope.footerIndex = 0;
+		$scope.footerIndex = 1;
 		//控制分支内容的隐现
 		$scope.showOrHide = function(x) {
 			if(x.value == "1") {
-				$scope.footerIndex = 0;
-			} else {
 				$scope.footerIndex = 1;
+			} else {
 				var idBottom = "#idBottom" + x.value;
 				var idTop = "#idTop" + x.value;
 				if($(idBottom).css('display') == 'block') {
@@ -34,10 +38,18 @@
 					$(idTop).removeClass("ion-ios-arrow-down");
 					$(idTop).addClass("ion-ios-arrow-up");
 				}
+				if (x.value == "2") {
+					$scope.footerIndex = 2;
+				} else{
+					$scope.footerIndex = 3;
+				}
 			}
 
 		}
 		
+		$scope.showFooter = function(y){
+			$scope.buttonData = $scope.buttonDataAll.slice(0,y.value); 
+		}
 		
 
 	})
