b8f78c19c4dc37d1e7c6197cfcc22c24aa2ef9fd
wang_szhan <wangshizhan1993@163.com>
2017-10-15 15:45:34 +0800

    1

---
+++ www/templates/home/componentList/details/js/carouselFigure.js
@@ -0,0 +1,42 @@
+/**
+ * @author wang_szhan
+ * @version 1.0.0
+ * @createDate 2017-10-12
+ * @content 各种类型的弹框
+ */
+(function() {
+	'use strict'
+
+	app.controller('carouselFigureCtrl', function($scope, $http, $stateParams, $ionicPopup, $timeout, toastService, publicService, $ionicActionSheet, $ionicHistory, $ionicModal,jQuerySlide) {
+		$scope.componentTitle = $stateParams.componentTitle;
+		$scope.componentData = $stateParams.componentData;
+
+		function getData(data) {
+			$scope.dataList = data;
+			console.log($scope.dataList);
+		}
+		publicService.receiveJson(getData, $scope.componentData);
+
+		function getData1(data) {
+			$scope.buttonDataAll = data;
+		}
+		publicService.receiveJson(getData1, "buttonData");
+
+		$scope.idBottom = "";
+		$scope.idTop = "";
+		//控制分支内容的隐现
+		$scope.showOrHide = function(x) {
+			jQuerySlide.slideUp($scope.idBottom,$scope.idTop);
+			$scope.buttonData = [];
+			if(x.value == "1") {
+				$scope.footerIndex = 1;
+			} else {
+				$scope.idBottom = "#idBottom" + x.value;
+				$scope.idTop = "#idTop" + x.value;
+				jQuerySlide.slideToggle($scope.idBottom,$scope.idTop);
+			}
+
+		}
+
+	})
+}())
