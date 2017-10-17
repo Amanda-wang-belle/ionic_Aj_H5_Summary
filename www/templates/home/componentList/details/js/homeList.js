5e7ac8e92dfcb3ea68da71ae78405bc879d1f82b
wang_szhan <wangshizhan1993@163.com>
2017-10-17 09:19:28 +0800

    1

--- www/templates/home/componentList/details/js/homeList.js
+++ www/templates/home/componentList/details/js/homeList.js
@@ -7,7 +7,7 @@
 (function() {
 	'use strict'
 
-	app.controller('footerCtrl', function($scope, $http, $stateParams, $ionicPopup, $timeout, toastService, publicService, $ionicActionSheet, $ionicHistory, $ionicModal,jQuerySlide) {
+	app.controller('homeListCtrl', function($scope, $http, $stateParams, $ionicPopup, $timeout, toastService, publicService, $ionicActionSheet, $ionicHistory, $ionicModal,jQuerySlide) {
 		$scope.componentTitle = $stateParams.componentTitle;
 		$scope.componentData = $stateParams.componentData;
 
@@ -15,44 +15,5 @@
 			$scope.dataList = data;
 		}
 		publicService.receiveJson(getData, $scope.componentData);
-
-		function getData1(data) {
-			$scope.buttonDataAll = data;
-		}
-		publicService.receiveJson(getData1, "buttonData");
-
-		$scope.footerIndex = 1;
-		$scope.idBottom = "";
-		$scope.idTop = "";
-		//控制分支内容的隐现
-		$scope.showOrHide = function(x) {
-			jQuerySlide.slideUp($scope.idBottom,$scope.idTop);
-			$scope.buttonData = [];
-			if(x.value == "1") {
-				$scope.footerIndex = 1;
-			} else {
-				$scope.idBottom = "#idBottom" + x.value;
-				$scope.idTop = "#idTop" + x.value;
-				jQuerySlide.slideToggle($scope.idBottom,$scope.idTop);
-				showDifFooter(x.value);
-			}
-
-		}
-		//显示三个不同大页签的底部栏
-		function showDifFooter(value){
-			if(value == "2") {
-				$scope.footerIndex = 2;
-			} else {
-				$scope.footerIndex = 3;
-			}
-		}
-		
-		//显示不同的footer
-		$scope.showFooter = function(x, y) {
-			showDifFooter(x.value);
-			$scope.buttonData = $scope.buttonDataAll.slice(0, y.value);
-			console.log($scope.buttonData);
-		}
-
 	})
 }())
