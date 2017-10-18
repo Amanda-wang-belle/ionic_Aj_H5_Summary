4b9a6e29a54ebe0127a3c3da45d86a7988d973a4
wang_szhan <wangshizhan1993@163.com>
2017-10-18 17:30:27 +0800

    1

--- www/templates/home/componentList/details/js/downloader.js
+++ www/templates/home/componentList/details/js/downloader.js
@@ -18,12 +18,15 @@
 		$scope.Back = function() {
 			history.back(-1);
 		}
-
+		$scope.file = {
+			"name":"",
+			"value":""
+		}
 		$scope.createDownload = function(x) {
 			var options = {
 				method: "GET",
-				filename: "_downloads/"+x.name,
-				create:true
+				filename: "_downloads/" + x.name,
+				create: true
 			};
 			var url = x.value;
 			var dtask = plus.downloader.createDownload(url, options, function(d, status) {
@@ -41,6 +44,39 @@
 				}
 			});
 			dtask.start();
+			dtask.addEventListener("statechanged", function(task, status) {
+				if(!dtask) {
+					return;
+				}
+				switch(task.state) {
+					case 1: // 开始
+						$ionicLoading.show({
+							template: "开始下载..."
+						});
+						break;
+					case 2: // 已连接到服务器
+						$ionicLoading.show({
+							template: "链接到服务器..."
+						});
+						break;
+					case 3: // 已接收到数据
+						$ionicLoading.show({
+							//template: "已经下载：" + task.downloadedSize + "/" + task.totalSize
+							//							template: "已经下载：" + (task.downloadedSize/task.totalSize*100).toFixed(0)+"%"
+							template: "正在下载..."
+
+						});
+						break;
+					case 4: // 下载完成
+						$ionicLoading.show({
+							template: "下载完成！"
+						});
+						$timeout(function() {
+							$ionicLoading.hide();
+						}, 1000);
+						break;
+				}
+			})
 		}
 
 		$scope.getfile = function(x) {
@@ -53,6 +89,12 @@
 			}, function(e) {
 				$scope.createDownload(x);
 			});
+		}
+		
+		
+		//清除所有下载任务
+		$scope.clearall = function(){
+			plus.downloader.clear();
 		}
 
 	})
