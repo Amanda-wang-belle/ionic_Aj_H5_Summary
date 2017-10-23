6983be8e302c193456f85b51317157d2b7d975f1
wang_szhan <wangshizhan1993@163.com>
2017-10-23 16:07:49 +0800

    1

--- www/templates/home/componentList/details/js/headers.js
+++ www/templates/home/componentList/details/js/headers.js
@@ -19,43 +19,7 @@
 			history.back(-1);
 		}
 
-		function getData1(data) {
-			$scope.buttonDataAll = data;
-		}
-		publicService.receiveJson(getData1, "buttonData");
-
-		$scope.footerIndex = 1;
-		$scope.idBottom = "";
-		$scope.idTop = "";
-		//控制分支内容的隐现
-		$scope.showOrHide = function(x) {
-			jQuerySlide.slideUp($scope.idBottom, $scope.idTop);
-			$scope.buttonData = [];
-			if(x.value == "1") {
-				$scope.footerIndex = 1;
-			} else {
-				$scope.idBottom = "#idBottom" + x.value;
-				$scope.idTop = "#idTop" + x.value;
-				jQuerySlide.slideToggle($scope.idBottom, $scope.idTop);
-				showDifFooter(x.value);
-			}
-
-		}
-		//显示三个不同大页签的底部栏
-		function showDifFooter(value) {
-			if(value == "2") {
-				$scope.footerIndex = 2;
-			} else {
-				$scope.footerIndex = 3;
-			}
-		}
-
-		//显示不同的footer
-		$scope.showFooter = function(x, y) {
-			showDifFooter(x.value);
-			$scope.buttonData = $scope.buttonDataAll.slice(0, y.value);
-			console.log($scope.buttonData);
-		}
+		
 
 	})
 }())
