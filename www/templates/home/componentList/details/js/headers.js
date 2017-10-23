c77f19d5330bbc09a502b5e6f86d4f63488b0ea3
wang_szhan <wangshizhan1993@163.com>
2017-10-23 17:04:36 +0800

    1

--- www/templates/home/componentList/details/js/headers.js
+++ www/templates/home/componentList/details/js/headers.js
@@ -13,12 +13,41 @@
 
 		function getData(data) {
 			$scope.dataList = data;
+			console.log($scope.dataList);
 		}
 		publicService.receiveJson(getData, $scope.componentData);
 		$scope.Back = function() {
 			history.back(-1);
 		}
 
+
+		$scope.left =  "1";
+		$scope.title = "1";
+		$scope.showTitle = function(x){
+			switch(x){
+				case "1":
+					$scope.title = "1";
+					$scope.left =  "";
+					$scope.right =  "";
+					break;
+				case "2":
+					$scope.title = "1";
+					$scope.left =  "1";
+					$scope.right =  "";
+					break;
+				case "3":
+					$scope.title = "1";
+					$scope.left =  "1";
+					$scope.right =  "1";
+					break;
+				case "4":
+					$scope.title = "2"
+					break;
+				default:
+					break;
+					
+			}
+		}
 		
 
 	})
