81fbe8177bf78534cb805e7b5929f8965a0a410b
wang_szhan <wangshizhan1993@163.com>
2017-10-25 13:03:18 +0800

    1

--- www/templates/home/componentList/details/js/slideUpDown.js
+++ www/templates/home/componentList/details/js/slideUpDown.js
@@ -23,6 +23,10 @@
 		}
 
 		$scope.slide1 = function(){
+			$(".slide2").slideUp();
+			$(".slide3").slideUp();
+			$(".slide4").slideUp();
+			
 			if ($(".slide1").css("display")=="none") {
 				$(".slide1").slideDown(); // 下拉显示列表
 			} else{
@@ -30,6 +34,10 @@
 			}
 		}
 		$scope.slide2 = function(){
+			$(".slide1").slideUp();
+			$(".slide3").slideUp();
+			$(".slide4").slideUp();
+			
 			if ($(".slide2").css("display")=="none") {
 				$(".slide2").slideDown(); // 下拉显示列表
 			} else{
@@ -37,6 +45,10 @@
 			}
 		}
 		$scope.slide3 = function(){
+			$(".slide1").slideUp();
+			$(".slide2").slideUp();
+			$(".slide4").slideUp();
+			
 			if ($(".slide3").css("display")=="none") {
 				$(".slide3").slideDown(); // 下拉显示列表
 			} else{
@@ -44,12 +56,34 @@
 			}
 		}
 		$scope.slide4 = function(){
+			$(".slide1").slideUp();
+			$(".slide2").slideUp();
+			$(".slide3").slideUp();
+			
 			if ($(".slide4").css("display")=="none") {
 				$(".slide4").slideDown(); // 下拉显示列表
 			} else{
 				$(".slide4").slideUp(); // 收回下拉列表
 			}
 		}
+		$(document).click(function(){
+			$(".slide1").slideUp();
+			$(".slide2").slideUp();
+			$(".slide3").slideUp();
+			$(".slide4").slideUp();
+		})
+		$(".slide1Button").click(function(){
+			return false;
+		})
+		$(".slide2Button").click(function(){
+			return false;
+		})
+		$(".slide3Button").click(function(){
+			return false;
+		})
+		$(".slide4Button").click(function(){
+			return false;
+		})
 
 	})
 }())
