e6b0d912c15d2837bc59ee41f556c1ec29e1c99f
wang_szhan <wangshizhan1993@163.com>
2017-10-25 18:58:45 +0800

    1

--- www/templates/home/componentList/details/js/styleAll.js
+++ www/templates/home/componentList/details/js/styleAll.js
@@ -8,22 +8,41 @@
 	'use strict'
 
 	app.controller('styleAllCtrl', function($scope, $http, $stateParams, $ionicPopup, $timeout, toastService, publicService, $ionicActionSheet, $ionicHistory, $ionicModal, jQuerySlide) {
-		 
+
 		$scope.componentTitle = $stateParams.componentTitle;
 		$scope.componentData = $stateParams.componentData;
+		
+		
+		var nowTime = new Date();
+		$("#startDate").val(nowTime);
+		$("#endDate").val(nowTime);
+		$scope.dataList = {
+			"proName": "",
+			"proMoney": "0.00",
+			"sartDate": nowTime,
+			"month": "10",
+			"proNumbber": "18",
+			"endDate": nowTime,
+			"retMethod": "00",
+			"proRate": "0.000",
+			"height": "0",
+			"rate": "00"
+		}
 
 		function getData(data) {
 			$scope.dataList = data;
+			console.log($scope.dataList);
+			$("#startDate").val($scope.dataList.startDate);
+			$("#endDate").val($scope.dataList.endDate);
 		}
 		publicService.receiveJson(getData, $scope.componentData);
 		$scope.Back = function() {
 			history.back(-1);
 		}
-
-	
-
-	
-
+		
+		$scope.save = function(){
+			console.log($scope.dataList.proName);
+		}
 
 	})
 }())
