75e34bff5b418ceb2df1d36fbd0883ae57a85825
wang_szhan <wangshizhan1993@163.com>
2017-11-01 13:51:32 +0800

    1

--- www/templates/home/componentList/details/js/charts.js
+++ www/templates/home/componentList/details/js/charts.js
@@ -42,8 +42,10 @@
 					break;
 				case 3:
 					$scope.bar3D();
+					break;
 				case 4:
 					$scope.pie3D();
+					break;
 				default:
 					break;
 			}
@@ -212,7 +214,7 @@
 						}
 					},
 					series: [{
-						name:$scope.chartData.seriousName,
+						name: $scope.chartData.seriousName,
 						data: $scope.chartData.data
 					}]
 				});
@@ -231,6 +233,55 @@
 				showValues();
 			});
 		}
+		//3D饼状图
+		$scope.pie3D = function() {
+			$(function() {
+				$('#container').highcharts({
+					chart: {
+						type: 'pie',
+						options3d: {
+							enabled: true,
+							alpha: 45,
+							beta: 0
+						}
+					},
+					title: {
+						text: '2014年某网站不同浏览器访问量占比'
+					},
+					tooltip: {
+						pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+					},
+					plotOptions: {
+						pie: {
+							allowPointSelect: true,
+							cursor: 'pointer',
+							depth: 35,
+							dataLabels: {
+								enabled: true,
+								format: '{point.name}'
+							}
+						}
+					},
+					series: [{
+						type: 'pie',
+						name: '浏览器占比',
+						data: [
+							['Firefox', 45.0],
+							['IE', 26.8],
+							{
+								name: 'Chrome',
+								y: 12.8,
+								sliced: true,
+								selected: true
+							},
+							['Safari', 8.5],
+							['Opera', 6.2],
+							['Others', 0.7]
+						]
+					}]
+				});
+			});
 
+		}
 	})
 }())
